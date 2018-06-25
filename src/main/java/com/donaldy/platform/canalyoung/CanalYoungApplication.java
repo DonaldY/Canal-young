c4719bb818a85237991e469b7c56f5b3809f8f91
DonaldY <448641125@qq.com>
2018-06-25 19:10:52 +0800

    init

---
+++ src/main/java/com/donaldy/platform/canalyoung/CanalYoungApplication.java
@@ -0,0 +1,13 @@
+package com.donaldy.platform.canalyoung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanalYoungApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CanalYoungApplication.class, args);
+	}
+}
---
+++ src/test/java/com/donaldy/platform/canalyoung/CanalYoungApplicationTests.java
@@ -0,0 +1,16 @@
+package com.donaldy.platform.canalyoung;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CanalYoungApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
